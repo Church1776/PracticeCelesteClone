8cd13c936d655662c0d6a189e9239955098c27be
Church1776 <brassmajor@gmail.com>
2025-08-28 00:08:04 -0400

    Updated the File I/O section

--- source/main_game_lib.h
+++ source/main_game_lib.h
@@ -2,7 +2,10 @@
 #define MAIN_GAME_LIB_H
 
 #include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include <sys/stat.h>
+
 
 //################################################################################################################################
 //                                                            Defines
@@ -136,6 +139,127 @@ char* bump_alloc(BumpAllocator* alloc, size_t size){
 //                                                            File I/O
 //################################################################################################################################
 
+long long get_timestamp(char* file){
+    struct stat file_stat = {};
+    stat(file, &file_stat);
+    return file_stat.st_mtime;
+}
+
+bool file_exists(char* filePath){
+    SM_ASSERT(filepath, "No file path supplied!");
+    
+    auto file = fopen(filePath, "rb");
+    if(!file){
+        return false;
+    }
+    fclose(file);
+    
+    return true;
+}
+
+long get_file_size(char* filePath){
+    SM_ASSERT(filePath, "No file path supplied!");
+    
+    long fileSize = 0;
+    auto file = fopen(filePath, "rb");
+    if(!file){
+        SM_ERROR("Failed opening file: %s", filePath);
+        return 0;
+    }
+    
+    fseek(file, 0, SEEK_END);
+    fileSize = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    fclose(file);
+    
+    return fileSize;
+}
+
+char* read_file(char* filePath, int* fileSize, char* buffer){
+    
+    SM_ASSERT(filePath, "No file path supplied!");
+    SM_ASSERT(fileSize, "No file size supplied!");
+    SM_ASSERT(buffer, "No buffer supplied!");
+    
+    *fileSize = 0;
+    auto file = fopen(filePath, "rb");
+    if(!file){
+        SM_ERROR("Failed opening file: %s", filePath);
+        return nullptr;
+    }
+    
+    fseek(file, 0, SEEK_END);
+    *fileSize = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    
+    memset(buffer, 0, *fileSize + 1);
+    fread(buffer, sizeof(char), *fileSize, file);
+    fclose(file);
+    
+    return buffer;
+}
+
+char* read_file(char* filePath, int* fileSize, BumpAllocator* alloc){
+    
+    char* file = nullptr;
+    long bufferSize = get_file_size(filePath);
+    
+    if(bufferSize){
+        char* buffer = bump_alloc(alloc, bufferSize + 1);
+        file = read_file(filePath, fileSize, buffer);
+    }
+    
+    return file;
+}
+
+void write_file(char* filePath, char* buffer, int size){
+    
+    SM_ASSERT(filePath, "No file path supplied!");
+    SM_ASSERT(buffer, "No buffer supplied!");
+    auto file = fopen(filePath, "wb");
+    if(!file){
+        SM_ERROR("Failed opening file: %s", filePath);
+        return;
+    }
+    
+    fwrite(buffer, sizeof(char), size, file);
+    fclose(file);    
+}
+
+bool copy_file(char* fileName, char* outputName, char* buffer){
+    
+    int fileSize = 0;
+    char* data = read_file(fileName, &fileSize, buffer);
+    
+    auto outputFile = fopen(outputName, "wb");
+    if(!outputFile){
+        SM_ERROR("Failed opening file: %s", outputName);
+        return false;
+    }
+    
+    int result = fwrite(data, sizeof(char), fileSize, outputFile);
+    if(!result){
+        SM_ERROR("Failed opening file: %s", outputName);
+        return false;
+    }
+    
+    fclose(outputFile);
+    
+    return true;
+}
+
+bool copy_file(char* fileName, char* outputName, BumpAllocator* alloc){
+    
+    char* file = nullptr;
+    long bufferSize = get_file_size(fileName);
+    
+    if(bufferSize){
+        char* buffer = bump_alloc(alloc, bufferSize + 1);
+        return copy_file(fileName, outputName, buffer);
+    }
+    
+    return false;
+}
 
 
 #endif//MAIN_GAME_LIB_H
